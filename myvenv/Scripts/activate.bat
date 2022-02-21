da938911538b6dfed5258e11db0e362f6f7c1f9c
saiidhanna21 <hanna.saiid2003@gmail.com>
2022-02-21 12:46:37 +0200

    venv

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\saiid\desktop\heroku_sample\myvenv
+set VIRTUAL_ENV=C:\Users\saiid\Desktop\heroku_sample\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
